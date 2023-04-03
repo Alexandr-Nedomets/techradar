0671a1febe01a0dafe6f2609e83db1de63d4de22
Nedomets Alexandr <anedomets@yandex.ru>
2023-04-03 15:29:14 +0300

    Initial commit

---
+++ src/test/java/ru/hh/techradar/TechradarApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hh.techradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechradarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
